02fd8c3898211450fc6211380af1274da0d026a7
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-07-15 11:43:08 +0800

    feat: create get api for restaurant data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 import express from "express";
-import restaurantList from "./restaurants.js";
+import getRestaurantList from "./restaurants.js";
 
 const app = express();
 const PORT = 6900;
+
+app.get('/getRestaurantList', async (req, res) => {
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    const radius = req.query.radius;
+
+    const response = await getRestaurantList(lat, lon, radius);
+    res.send(response);
+})
 
 app.listen(PORT, () => {
     'app listening on port 6900'
