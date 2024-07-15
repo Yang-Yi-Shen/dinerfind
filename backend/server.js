ef154c5e4bf9f7f650f4347f665fd451f00a8666
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-07-15 11:20:22 +0800

    feat: set up express app

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
+import express from "express";
 import restaurantList from "./restaurants.js";
+
+const app = express();
+const PORT = 6900;
+
+app.listen(PORT, () => {
+    'app listening on port 6900'
+})
