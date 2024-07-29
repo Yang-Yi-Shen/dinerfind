85b308a150a7846fed673eea0bd406ffe01bc147
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-07-29 11:22:23 +0800

    feat: invert tag colors

--- frontend/index.js
+++ frontend/index.js
@@ -14,8 +14,8 @@ const restaurantListItems = restaurantData.map(restaurant =>
         <h2 class="restaurant-name text-lg font-bold">${restaurant.name}</h2>
         <p class="restaurant-address text-sm font-light text-slate-400">${restaurant.address}</p>
         <div class="restaurant-meta text-sm flex gap-2 mt-1">
-            <p class="restaurant-meta-item text-slate-50 border-none px-1 rounded-sm bg-slate-300">${restaurant.distance}m</p>
-            ${restaurant.cuisine !== undefined ? `<p class="restaurant-meta-item text-slate-50 border-none px-1 rounded-sm bg-slate-300">${restaurant.cuisine}</p>` : ''}
+            <p class="restaurant-meta-item text-slate-700 border-none px-1 rounded-sm bg-slate-200">${restaurant.distance}m</p>
+            ${restaurant.cuisine !== undefined ? `<p class="restaurant-meta-item text-slate-700 border-none px-1 rounded-sm bg-slate-200">${restaurant.cuisine}</p>` : ''}
         </div>
     </li>`).join('')
 restaurantList.innerHTML = restaurantListItems
