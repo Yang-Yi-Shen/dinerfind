1301a5e276a253bd98f119311679efbb889a1a9b
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-07-29 09:52:13 +0800

    feat: display data w/ inline html

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,17 @@ const userLatitude = 40.712670;
 const userLongitude = -73.995819;
 const radius = 500;
 
-const restaurantList = await fetch(`${url}/getRestaurantList?lat=${userLatitude}&lon=${userLongitude}&radius=${radius}`)
+const restaurantData = await fetch(`${url}/getRestaurantList?lat=${userLatitude}&lon=${userLongitude}&radius=${radius}`)
     .then(response => response.json())
 
-console.log(restaurantList)
+const restaurantList = document.getElementById('restaurant-list')
+const restaurantListItems = restaurantData.map(restaurant => 
+    `<li>
+        <h2 class="restaurant-name">${restaurant.name}</h2>
+        <p class="restaurant-address">${restaurant.address}</p>
+        <div class="restaurant-meta">
+            <p class="restaurant-meta-item">${restaurant.distance}</p>
+            <p class="restaurant-meta-item">${restaurant.cuisine}</p>
+        </div>
+    </li>`).join('')
+restaurantList.innerHTML = restaurantListItems
