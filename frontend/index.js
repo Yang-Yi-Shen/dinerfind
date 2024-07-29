e935ad4913e47e380c2af68607e5f34d35512c53
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-07-29 10:47:34 +0800

    feat: style restaurant element

--- frontend/index.js
+++ frontend/index.js
@@ -10,12 +10,12 @@ const restaurantData = await fetch(`${url}/getRestaurantList?lat=${userLatitude}
 
 const restaurantList = document.getElementById('restaurant-list')
 const restaurantListItems = restaurantData.map(restaurant => 
-    `<li>
-        <h2 class="restaurant-name">${restaurant.name}</h2>
-        <p class="restaurant-address">${restaurant.address}</p>
-        <div class="restaurant-meta">
-            <p class="restaurant-meta-item">${restaurant.distance}</p>
-            <p class="restaurant-meta-item">${restaurant.cuisine}</p>
+    `<li class="restaurant border-t last:border-b  px-4 pt-2 pb-3">
+        <h2 class="restaurant-name text-lg font-bold">${restaurant.name}</h2>
+        <p class="restaurant-address text-sm font-light text-slate-400">${restaurant.address}</p>
+        <div class="restaurant-meta text-sm flex gap-2 mt-1">
+            <p class="restaurant-meta-item text-slate-50 border-none px-1 rounded-sm bg-slate-300">${restaurant.distance}</p>
+            <p class="restaurant-meta-item text-slate-50 border-none px-1 rounded-sm bg-slate-300">${restaurant.cuisine}</p>
         </div>
     </li>`).join('')
 restaurantList.innerHTML = restaurantListItems
